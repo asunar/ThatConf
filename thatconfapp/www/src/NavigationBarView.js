0ac52b7df0c90940bd5bfeb5c3284de24365a74c
Alper Sunar <ASunar@paylocity.com>
2014-09-10 14:11:19 -0500

    Refactored NavigationBarView

--- thatconfapp/www/src/NavigationBarView.js
+++ thatconfapp/www/src/NavigationBarView.js
@@ -9,12 +9,11 @@ var NavigationBarView = function(adapter) {
 				var template = 
 			"<table style='width:100%;height:100%; text-align:center'>"
 		+		'<tr>'
-		+     "<td onclick=" + "window.location='{{ url }}'><a class='navBarItem' href='#home'><i class='fa fa-home fa-w fa-2x'></i></a></td>"
+		+     "<td class='navBarCell' data-url='#home' >"
+		+			"<i class='fa fa-home fa-w fa-2x'></i></td>"
 		+		"{{# . }}"	
-		+			"<td onclick=" + "window.location='{{ url }}'" + ">"
-		+			"<a href={{ url }} class='navBarItem' >"	
+		+			"<td  class='navBarCell' data-url='{{ url }}'>"
 		+			"<i class='{{ icon }} fa-w fa-2x'></i>"
-		+			"</a>"
 		+			"</td>"
 		+		"{{/.}}"	
 		+		'</tr>'
@@ -22,10 +21,26 @@ var NavigationBarView = function(adapter) {
 
 			 menuTemplate = Handlebars.compile(template)	
        this.el.innerHTML = menuTemplate(adapter.getNavigationBarItems());
+			 this.wireUpEvents();
        return this.el;
     };
 
+		this.wireUpEvents = function() {
+			var navBarCells = this.el.getElementsByClassName('navBarCell');
+			var buttonNodes = this.el.getElementsByTagName('i');
 
+			// convert nodelist into an array
+			var navButtons = Array.prototype.slice.call(buttonNodes, 0);
+			var cells = Array.prototype.slice.call(navBarCells, 0);
+			cells.forEach(function(cell){
+				cell.onclick = function(e){
+					window.location.href = this.dataset.url;
+					cells.forEach(function(c){ c.classList.remove('selectedNavBarItem');c.children[0].classList.remove('selectedIcon');  })
+					cell.classList.toggle('selectedNavBarItem');
+					cell.children[0].classList.toggle('selectedIcon')
+				}
+			});
+		};
 
     this.initialize();
 };
